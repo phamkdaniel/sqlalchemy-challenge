cdf34438f72df4c73b403944a5f8e7a493cc9a2b
phamkdaniel <phamkdaniel@gmail.com>
2019-10-31 15:12:25 -0700

    update return type

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify
-from sqlquery import read
-from sqlquery import final_entry
-
+from sqlquery import read, calc_temps, get_tobs, final_entry
 
 # create instance of Flask
 app = Flask(__name__)
@@ -28,39 +26,22 @@ def precipitation():
 # stations page
 @app.route('/api/v1.0/stations')
 def stations():
-    query = 'SELECT * FROM station'
+    query = 'SELECT station.station, station.name FROM station'
     result = read(query)
     return jsonify(dict(result))
 
 # temperature observations page
 @app.route('/api/v1.0/tobs')
 def tobs():
-    from sqlquery import get_tobs
     temp_obs = get_tobs(final_entry.date)
     return jsonify(dict(temp_obs))
 
 # calculates min, avg, max temp between start and end date
 # if no end date, calculates for all dates >= start date
 @app.route('/api/v1.0/<start>')
-def start_dates(start):
-    from sqlquery import calc_temps
-    val = calc_temps(start, final_entry.date)
-    print(f"LOG {final_entry.date}")
-    summary = {"min" : val[0], 
-        "avg" : val[1], 
-        "max" : val[2]
-    }
-    return jsonify(summary)
-
-
 @app.route('/api/v1.0/<start>/<end>')
-def dates(start, end):
-    from sqlquery import calc_temps
+def dates(start, end=final_entry.date):
     val = calc_temps(start, end)
-    print(f"LOG {val[0]}")
-    print(f"LOG {val[1]}")
-    print(f"LOG {val[2]}")
-
     summary = {"min" : val[0], 
         "avg" : val[1], 
         "max" : val[2]
